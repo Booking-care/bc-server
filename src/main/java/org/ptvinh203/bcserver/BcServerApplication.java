0f0ad941dbac820dcdeaf56cc59850eaaae1ab00
Thanh Vinh <ptvinh203@gmail.com>
2024-03-14 21:47:55 +0700

    Initial project

---
+++ src/main/java/org/ptvinh203/bcserver/BcServerApplication.java
@@ -0,0 +1,13 @@
+package org.ptvinh203.bcserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BcServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ptvinh203/bcserver/BcServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ptvinh203.bcserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
